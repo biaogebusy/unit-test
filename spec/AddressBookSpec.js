fb1b03d1997f518186f211a65040e14c2b2e0e9b
biaogebusy <349255833@qq.com>
2018-11-24 16:03:53 +0800

    beforeEach function

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -2,22 +2,22 @@
  * 用describe建立Address Book测试组
  */
 describe('Address Book', function () {
+
+  // 每一组测试可能包含相同的代码，可以放入到beforeEach()函数中
+  // 每组测试在运行前线运行里面的代码
+  beforeEach(function () {
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  })
   // 用it一组测试要求
   it('should be able to add a contack', function () {
-    var addressBook = new AddressBook();
-    var thisContact = new Contact();
-
     addressBook.addContact(thisContact);
-
     // 测试获取第一个地址是否是传进来的实例
     expect(addressBook.getContact(0)).toBe(thisContact);
   });
 
   // 建立另一组测试要求
   it('should be able to delete a contact', function () {
-    var addressBook = new AddressBook();
-    var thisContact = new Contact();
-
     // 添加联系方式到通讯录
     addressBook.addContact(thisContact);
     // 删除指定通讯录
